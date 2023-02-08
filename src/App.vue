a97146d19fcef8b1ac1a3e91b4333c67a5cb20b5
liuyingming <yingming006@foxmail.com>
2023-02-08 15:30:08 +0800

    first push

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <el-popover :width="550" trigger="click">
+		<template #reference>
+			<el-input placeholder="请输入"></el-input>
+		</template>
+		<el-select multiple placeholder="Select">
+			<el-option key="1" label="label1" value="v1" />
+      <el-option key="2" label="label2" value="v2" />
+      <el-option key="3" label="label3" value="v3" />
+		</el-select>
+	</el-popover>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
